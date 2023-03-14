c35d00a9c903bca4df9ffb71b4f2f6b8eb17b577
Donald Besong <donald.besong.principles@gmail.com>
2023-03-14 11:35:59 +0000

    trying to get the public ip to work on the browser

--- app_to_run.py
+++ app_to_run.py
@@ -1,5 +1,5 @@
 from app import create_app
 app = create_app('default')
-#windows cmd ren gitignore.txt .gitignore
+#windows cmd ren gitignore.txt .gitignore #
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
